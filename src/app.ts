82aab6b36f62854e9be885ca37512b5e87b648b5
mvrk <oscarmenacho23@gmail.com>
2023-01-19 03:12:48 -0500

    fix port

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import router from './routes/diaries'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 app.use(express.json())
 app.use('/api/diaries', router)
